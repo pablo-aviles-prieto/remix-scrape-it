00508b368f60575b3321dbd73a869dd75dae5858
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-06-25 21:39:50 +0200

    feat(): Add og metadata

--- app/utils/create-base-metadata-info.ts
+++ app/utils/create-base-metadata-info.ts
@@ -1,18 +1,24 @@
 import type { MetaFunction } from '@remix-run/node';
 import { stores } from './const';
 
-// TODO: Add OG to the metadata base
 export const createBaseMetadataInfo: MetaFunction = () => {
   const supportedStores = Object.values(stores);
   const storeList = supportedStores.join(', ');
+  const description = `ScrapeIt! te permite rastrear y seguir los precios de los productos de tus tiendas favoritas como ${storeList}. Crea seguimientos para tus productos favoritos y recibe actualizaciones diarias por correo electrónico sobre los últimos precios o cuando el producto llegue al precio que deseas.`;
+  const titleData =
+    'ScrapeIt! - Consigue los mejores precios de tus tiendas favoritas';
+
   return [
+    { title: titleData },
+    { name: 'description', content: description },
+    { property: 'og:title', content: titleData },
+    { property: 'og:description', content: description },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://www.scrapeit.pabloaviles.es/' },
     {
-      title:
-        'ScrapeIt! - Consigue los mejores precios de tus tiendas favoritas',
-    },
-    {
-      name: 'description',
-      content: `ScrapeIt! te permite rastrear y seguir los precios de los productos de tus tiendas favoritas como ${storeList}. Crea seguimientos para tus productos favoritos y recibe actualizaciones diarias por correo electrónico sobre los últimos precios o cuando el producto llegue al precio que deseas.`,
+      property: 'og:image',
+      content:
+        'https://www.scrapeit.pabloaviles.es/images/scrapeit_social_network.webp',
     },
   ];
 };
