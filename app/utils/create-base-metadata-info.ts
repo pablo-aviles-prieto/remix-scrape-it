24261680a2475255ac881188055ea1f503fca8da
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2025-04-20 15:06:45 +0200

    update og url domain

--- app/utils/create-base-metadata-info.ts
+++ app/utils/create-base-metadata-info.ts
@@ -5,8 +5,7 @@ export const createBaseMetadataInfo: MetaFunction = () => {
   const supportedStores = Object.values(stores);
   const storeList = supportedStores.join(', ');
   const description = `ScrapeIt! te permite rastrear y seguir los precios de los productos de tus tiendas favoritas como ${storeList}. Crea seguimientos para tus productos favoritos y recibe actualizaciones diarias por correo electrónico sobre los últimos precios o cuando el producto llegue al precio que deseas.`;
-  const titleData =
-    'ScrapeIt! - Consigue los mejores precios de tus tiendas favoritas';
+  const titleData = 'ScrapeIt! - Consigue los mejores precios de tus tiendas favoritas';
 
   return [
     { title: titleData },
@@ -14,11 +13,10 @@ export const createBaseMetadataInfo: MetaFunction = () => {
     { property: 'og:title', content: titleData },
     { property: 'og:description', content: description },
     { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: 'https://www.scrapeit.pabloaviles.es/' },
+    { property: 'og:url', content: 'https://www.scrapeit.pabloaviles.dev/' },
     {
       property: 'og:image',
-      content:
-        'https://www.scrapeit.pabloaviles.es/images/scrapeit_social_network.webp',
+      content: 'https://www.scrapeit.pabloaviles.dev/images/scrapeit_social_network.webp',
     },
   ];
 };
